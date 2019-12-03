c9efaefd42feeafb2e71ff0b6797922b16c6c985
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-03 10:10:12 +0200

    Updates

---
+++ precache-manifest.e50f8a86c489878d1104236856181f56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3e82291b9af51fe0696a84b3962494",
+    "url": "/cra-demo/index.html"
+  },
+  {
+    "revision": "6ea624aa9b9697035ed0",
+    "url": "/cra-demo/static/js/2.a3e8d21e.chunk.js"
+  },
+  {
+    "revision": "e1da135b4cb63fb2bd17",
+    "url": "/cra-demo/static/js/main.da95eed8.chunk.js"
+  },
+  {
+    "revision": "51dd5d735150a0d15256",
+    "url": "/cra-demo/static/js/runtime-main.30833128.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cra-demo/precache-manifest.b74812e9ab6c5a3778565dc17ee09c9e.js"
+  "/cra-demo/precache-manifest.e50f8a86c489878d1104236856181f56.js"
 );
 
 self.addEventListener('message', (event) => {
